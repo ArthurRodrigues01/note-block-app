c1a9596ca9405d3a4f88159a94e9f75de26e277f
Arthur Rodrigues <art.costa.2005@gmail.com>
2022-05-04 20:13:42 -0300

    Add

--- src/components/Main/NotesArea/index.js
+++ src/components/Main/NotesArea/index.js
@@ -1,7 +1,41 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { View } from 'react-native';
 
-const notes = AsyncStorage.getItem('notes');
+// const notes = AsyncStorage.getItem('notes');
+
+// const notes = {
+//     notes: [
+//         {
+//             id: 0,
+//             title: 'titulo 1',
+//             text: 'nota 1'
+//         },
+//         {
+//             id: 1,
+//             title: 'titulo 2', 
+//             text: 'nota 2'
+//         },
+//         {
+//             id: 2,
+//             title: 'titulo 3', 
+//             text: 'nota 3'
+//         },
+//         {
+//             id: 3,
+//             title: 'titulo 4', 
+//             text: 'nota 4'
+//         },
+//         {
+//             id: 4,
+//             title: 'titulo 5', 
+//             text: 'nota 5'
+//         },
+//     ]
+// }
+// console.log(Object.keys(notes))
+// for(i=0, i<notes.notes.length; i++){
+
+// }
 
 export default () => 
 <View>
